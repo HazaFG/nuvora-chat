2e2d2767f0ee8d2b93d1b9b8f61863d4ee2831e6
HazaFG <hazaelfg@gmail.com>
2025-05-28 01:02:25 -0700

    rutas login y register

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <>
+      <div>
+        Hola, esto es el login page
+      </div>
+    </>
+  )
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <>
+      <div>
+        Hola, esto es el register page
+      </div>
+    </>
+  )
+}
