86e9e660da8443b55f05b462533b30e76cb51a20
HazaFG <hazaelfg@gmail.com>
2025-05-31 13:40:34 -0700

    sidebar de flowbite ingresado, host de flowbite configurado

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "@/components";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
@@ -26,6 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Sidebar />
+
         {children}
       </body>
     </html>
