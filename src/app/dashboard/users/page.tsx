fe714a82f93899a060b6291d6454840e0e2f34b8
AgusAbaroa2001 <abaroa2@gmail.com>
2025-06-02 22:37:07 -0700

    Agregar Pagina Usuario

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -1,12 +1,16 @@
+import { User } from "@/components";
+
 export default function ProfilePage() {
   return (
     <>
       <div className="p-4 sm:ml-64">
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
           <div className="bg-background text-foreground font-madimi">
+            <User></User>
           </div>
         </div>
       </div>
     </>
   )
 }
+
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Login } from './Login'
 export { Register } from './Register'
 export { Sidebar } from './Sidebar'
+export { User} from './User'
