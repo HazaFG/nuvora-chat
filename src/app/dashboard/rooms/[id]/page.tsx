43fe49d0fe72d8bcaf42b87be2e90e9dc9c757d9
MarcosIgnacioo <magma_ruby145@hotmail.com>
2025-06-06 20:57:05 -0700

    typescript error fix

--- src/app/dashboard/rooms/[id]/page.tsx
+++ src/app/dashboard/rooms/[id]/page.tsx
@@ -1,18 +1,15 @@
 import ChatTemplate from "@/components/ChatTemplate"
-import Spinner from "@/components/Spinner"
 
 export default async function Page({
   params,
-  searchParams,
 }: {
-  params: Promise<{ slug: string }>
+  params: Promise<{ id: string }>
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
   const { id } = await params
 
   return (
     <>
-
       <div className="p-4 sm:ml-64 h-screen">
         <div className="h-full">
           <ChatTemplate roomId={id} />
