c192fbf66ea33bc96e3610fb83fda52dc9925184
MarcosIgnacioo <magma_ruby145@hotmail.com>
2025-06-06 20:52:31 -0700

    room list image

--- src/app/dashboard/rooms/page.tsx
+++ src/app/dashboard/rooms/page.tsx
@@ -11,12 +11,17 @@ export default async function Rooms() {
       <div className="p-4 sm:ml-64 h-screen">
         <div className="h-full">
           <ul className="space-y-2 font-medium">
-            {rooms.map(room =>
+            {rooms.map((room: any) =>
               <li key={`${room.name}-${room.id}`}>
                 <Link
                   href={`./rooms/${room.id}`}
                   className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group sidebar-menu-item-link`}
                 >
+                  <img
+                    src={(room.image) ? `data:image/png;base64,${room.image}` : '/cloudWhite.png'}
+                    alt="Logo"
+                    className="w-10 h-10 rounded-full object-cover flex-shrink-0"
+                  />
                   <span className="ms-3" style={{ color: 'var(--sidebar-text)' }}>{room.name}</span>
                 </Link>
               </li>)
