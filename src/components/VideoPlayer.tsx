d897ef869f83808504a586a414df838834c7fa1d
HazaFG <hazaelfg@gmail.com>
2025-06-06 18:25:23 -0700

    video player mejorado

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,10 +1,18 @@
-"use client"
+"use client";
 export default function VideoPlayer(props: any) {
-  const src = `data:video/mp4;base64,${props.src}`
+  const src = `data:video/mp4;base64,${props.src}`;
+
   return (
-    <video width="320" height="240" controls preload="none">
-      <source src={src} type="video/mp4" />
-    </video>
-  )
+    <div className="flex flex-col items-center gap-2 p-2 rounded-lg bg-gray-100 dark:bg-gray-700 w-[340px] max-w-full mx-auto shadow-sm">
+      <video
+        controls
+        preload="metadata"
+        className="rounded-md w-full h-auto"
+      >
+        <source src={src} type="video/mp4" />
+        Tu navegador no soporta el video.
+      </video>
+    </div>
+  );
 }
 
